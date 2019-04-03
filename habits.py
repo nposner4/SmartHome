f306bfe2e190999458c5e9292930d5161a59c01b
Noah Posner <33390777+nposner4@users.noreply.github.com>
2019-04-03 22:50:32 +0200

    Track users eating habits
    use k-means clustering to predict when a user eats breakfast,lunch and dinner

---
+++ habits.py
@@ -0,0 +1,47 @@
+import readInJson
+
+
+import datetime
+
+import matplotlib
+import matplotlib.pyplot as plt
+import matplotlib.dates as dates
+import matplotlib.gridspec as gridspec
+import numpy as np
+import pandas as pd
+import datetime
+from sklearn.cluster import KMeans
+from matplotlib.dates import DayLocator, HourLocator, DateFormatter, drange
+matplotlib.style.use('ggplot')
+
+
+def find_habits(data, ke, n_cluster):
+	temp = []
+	for key in ke:
+		temp = data[(key, "accelx")]
+		print("quick brown fox")
+		print(temp)
+	cluster = []
+	for index, entry in temp.iterrows():
+		cluster.append([entry[0], float(str(entry[1])[11:13])])
+	data = temp
+	print(cluster)
+	kmeans = KMeans(n_clusters = n_cluster)
+	
+	kmeans.fit(cluster)
+	labels = kmeans.predict(cluster)
+	centers = kmeans.cluster_centers_
+	
+	print(centers)
+	print("centers have been printed")
+	z = [t[::-1] for t in centers]
+	
+	plt.scatter(*zip(*z))
+	plt.title('Fridge')
+	plt.xlabel('Time')
+	plt.ylabel('Acceleration')
+	plt.show()
+	
+	
+		
+
